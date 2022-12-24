4b466e7a9633a9c91eb46774586806f8709dc8db
Aman Lachhiramka <amanlachhiramka09@gmail.com>
2022-12-24 16:54:46 +0530

    react-testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, {useRef} from 'react'
 import './App.css';
 
 function App() {
+
+  let inputRef=useRef(null);
+
+  function handleName(){
+      inputRef.current.value="Aman"
+      inputRef.current.style.color="black"
+  }
+
+  function handleNameRed(){
+      inputRef.current.style.color="red"
+  }
+
+  function handleNameBlue(){
+      inputRef.current.style.color="blue"
+  }
+
+  function handleErase(){
+      inputRef.current.value=""
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Demonstrating "useRef hook" in React</h1>
+      <hr />
+      <input id="inputField" class="inputField"type="text" ref={inputRef}/>
+      <button id="button1" onClick={handleName}>Show my name</button>
+      <button id="button2" onClick={handleNameRed}>Turn it red</button>
+      <button id="button3" onClick={handleNameBlue}>Turn it blue</button>
+      <button id="button4" onClick={handleErase}>Erase it</button>
     </div>
   );
 }
